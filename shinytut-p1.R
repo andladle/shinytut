6fc3d9d1ffe9fd1ceb80ea03237a2c575af275d3
Andrew Ladle <andrew.ladle@gov.bc.ca>
2023-09-13 15:31:22 -0700

    Created some shiny apps

--- shinytut-p1.R
+++ shinytut-p1.R
@@ -4,3 +4,35 @@
 #' output: github_document
 #' ---
 
+#' First step is to load the Shiny app
+library(shiny)
+
+#' Next create directory for the app - this should be in a standalone R script
+
+ui <- fluidPage(
+  "Hello, world!"
+)
+
+server <- function(input, output, session) {
+}
+
+shinyApp(ui, server)
+
+#' I see that RStudio instantly recognises that a Shiny app is included and 
+#' offers the "Run App" option
+#' 
+#' Now we make it more compelx and add UI
+#' 
+ui <- fluidPage(
+  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
+  verbatimTextOutput("summary"),
+  tableOutput("table")
+)
+
+server <- function(input, output, session) {
+}
+
+shinyApp(ui, server)
+
+#'  This has created a UI, which is a drop down menu showing a list of 
+#'  dataset choices
